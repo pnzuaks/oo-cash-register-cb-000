da38b97246a0d27cdea37650574ac14f030c3067
Patricia Nykaza <pnykaza@calstatela.edu>
2018-06-25 21:24:15 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,8 +17,7 @@ class CashRegister
 
   def apply_discount()
     percent_off = self.discount.to_f / 100.0 
-    percent_off * @total
-    @total - 
+    @total - (percent_off * @total)
   end
   
   def items()
