d42d4004c0c51cca9f965730488ffc5db64766ed
Patricia Nykaza <pnykaza@calstatela.edu>
2018-06-25 20:56:46 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   end
   
   def add_item(title, price, quantity = 1)
-    result = self.total + (price.to_f*quantity.to_i)
+    result = self.total + (price.to_f * quantity.to_i)
   end
 
   def apply_discount()
