8bf3ce59bf8f4b396ae24506e10c99e4246f86d1
Patricia Nykaza <pnykaza@calstatela.edu>
2018-06-25 21:26:47 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
   def apply_discount()
     percent_off = self.discount.to_f / 100.0 
     @total = @total - (percent_off * @total)
-    puts "After the discount, the total comes to $#{@total}."
+    return "After the discount, the total comes to $#{@total}."
   end
   
   def items()
